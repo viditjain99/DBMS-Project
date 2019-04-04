e60bc4137cd2c4038824b8c4988d323e7781dc8d
Vidit Jain <vidit1000@gmail.com>
2019-04-04 12:47:20 +0530

    Removed GUI

--- BookStore.java
+++ BookStore.java
@@ -1,155 +1,59 @@
-import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.geometry.Rectangle2D;
-import javafx.scene.Group;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.Tab;
-import javafx.scene.control.TabPane;
-import javafx.scene.layout.*;
-import javafx.scene.text.Font;
-import javafx.scene.text.Text;
-import javafx.stage.Screen;
-import javafx.stage.Stage;
-
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.util.Stack;
+import java.sql.SQLException;
+import java.util.Scanner;
 
-public class BookStore extends Application
+public class BookStore
 {
-    static Stack<Scene> sceneStack=new Stack<>();
-    static Connection connection;
-    @Override
-    public void start(Stage primaryStage) throws Exception
+    public static void main(String[] args) throws SQLException
     {
-        connection=DriverManager.getConnection("jdbc:mysql://localhost:3306/BookStore","root","password123");
-        System.out.println(connection);
-        Group root=new Group();
-        Rectangle2D screenDimens= Screen.getPrimary().getBounds();
-        Scene homePage=new Scene(root,screenDimens.getWidth(),screenDimens.getHeight());
-        Text titleTextView=new Text("Book Store");
-        titleTextView.setFont(Font.font("verdana",50));
-        titleTextView.setLayoutX(screenDimens.getWidth()/2-50);
-        titleTextView.setLayoutY(50);
-        Button backButton=new Button("Back");
-        backButton.setAlignment(Pos.TOP_LEFT);
-        backButton.setPadding(new Insets(10,10,10,10));
-
-        backButton.setOnAction(new EventHandler<ActionEvent>()
+        Scanner scanner=new Scanner(System.in);
+        Connection connection=DriverManager.getConnection("jdbc:mysql://localhost:3306/BookStore","root","password123");
+        System.out.println("1. Books Database");
+        System.out.println("2. Employees Database");
+        System.out.println("3. Customer Database");
+        System.out.println("4. Exit");
+        System.out.print("Enter Choice: ");
+        int choice=scanner.nextInt();
+        while(choice!=4)
         {
-            @Override
-            public void handle(ActionEvent event)
+            if(choice==1)
             {
-                sceneStack.pop();
-                primaryStage.setScene(sceneStack.peek());
+                int booksDatabaseChoice;
+                System.out.println("---------------Books Database---------------");
+                System.out.println();
+                System.out.println("1. Add Book");
+                System.out.println("2. Sell Book");
+                System.out.println("3. Search Book");
+                System.out.println("4. Search Book in other Branches");
+                System.out.println("5. Modify Book Details");
+                System.out.print("Enter Choice: ");
             }
-        });
-        Button booksDatabaseButton=new Button("Books Database");
-        booksDatabaseButton.setLayoutX(screenDimens.getWidth()/2);
-        booksDatabaseButton.setLayoutY(screenDimens.getHeight()/2-100);
-        booksDatabaseButton.setOnAction(new EventHandler<ActionEvent>()
-        {
-            @Override
-            public void handle(ActionEvent event)
+            else if(choice==2)
             {
-                AnchorPane root=new AnchorPane();
-                Scene booksDatabaseScene=new Scene(root,screenDimens.getWidth(),screenDimens.getHeight());
-                TabPane tabPane=new TabPane();
-                tabPane.setTabClosingPolicy(TabPane.TabClosingPolicy.UNAVAILABLE);
-                for(int i=0;i<5;i++)
-                {
-                    Tab tab=new Tab("tab "+(i+1));
-                    HBox hBox=new HBox();
-                    hBox.getChildren().add(new Label("Tab "+(i+1)));
-                    hBox.setAlignment(Pos.TOP_LEFT);
-                    tab.setContent(hBox);
-                    tabPane.getTabs().add(tab);
-                }
-                AnchorPane.setTopAnchor(tabPane,5.0);
-                AnchorPane.setLeftAnchor(tabPane,5.0);
-                AnchorPane.setRightAnchor(tabPane,5.0);
-                AnchorPane.setTopAnchor(backButton,5.0);
-                AnchorPane.setRightAnchor(backButton,5.0);
-                root.getChildren().addAll(tabPane,backButton);
-                primaryStage.setScene(booksDatabaseScene);
-                sceneStack.push(booksDatabaseScene);
+                int employeesDatabaseChoice;
+                System.out.println("---------------Employees Database---------------");
+                System.out.println();
+                System.out.println("1. Search Employee");
+                System.out.println("2. Search for Employees in a Branch");
+                System.out.println("3. Add Employee");
+                System.out.println("4. Remove Employee");
+                System.out.println("5. Modify Employee Details");
+                System.out.print("Enter Choice: ");
             }
-        });
-        Button customerDatabaseButton=new Button("Customer Database");
-        customerDatabaseButton.setLayoutX(screenDimens.getWidth()/2);
-        customerDatabaseButton.setLayoutY(screenDimens.getHeight()/2);
-        customerDatabaseButton.setOnAction(new EventHandler<ActionEvent>()
-        {
-            @Override
-            public void handle(ActionEvent event)
+            else if(choice==3)
             {
-                AnchorPane root=new AnchorPane();
-                Scene customerDatabaseScene=new Scene(root,screenDimens.getWidth(),screenDimens.getHeight());
-                TabPane tabPane=new TabPane();
-                tabPane.setTabClosingPolicy(TabPane.TabClosingPolicy.UNAVAILABLE);
-                for(int i=0;i<5;i++)
-                {
-                    Tab tab=new Tab("tab "+(i+1));
-                    HBox hBox=new HBox();
-                    hBox.getChildren().add(new Label("Tab "+(i+1)));
-                    hBox.setAlignment(Pos.TOP_LEFT);
-                    tab.setContent(hBox);
-                    tabPane.getTabs().add(tab);
-                }
-                AnchorPane.setTopAnchor(tabPane,5.0);
-                AnchorPane.setLeftAnchor(tabPane,5.0);
-                AnchorPane.setRightAnchor(tabPane,5.0);
-                AnchorPane.setTopAnchor(backButton,5.0);
-                AnchorPane.setRightAnchor(backButton,5.0);
-                root.getChildren().addAll(tabPane,backButton);
-                primaryStage.setScene(customerDatabaseScene);
-                sceneStack.push(customerDatabaseScene);
+                int customersDatabaseChoice;
+                System.out.println("---------------Customers Database---------------");
+                System.out.println();
+                System.out.println("1. Search Customer");
+                System.out.println("2. Add New Customer");
+                System.out.println("3. Delete Customer");
+                System.out.println("4. Modify Customer Details");
+                System.out.println("5. Return Book");
+                System.out.print("Enter Choice: ");
             }
-        });
-        Button employeesDatabaseButton=new Button("Employees Database");
-        employeesDatabaseButton.setLayoutX(screenDimens.getWidth()/2);
-        employeesDatabaseButton.setLayoutY(screenDimens.getHeight()/2+100);
-        employeesDatabaseButton.setOnAction(new EventHandler<ActionEvent>()
-        {
-            @Override
-            public void handle(ActionEvent event)
-            {
-                AnchorPane root=new AnchorPane();
-                Scene employeesDatabaseScene=new Scene(root,screenDimens.getWidth(),screenDimens.getHeight());
-                TabPane tabPane=new TabPane();
-                tabPane.setTabClosingPolicy(TabPane.TabClosingPolicy.UNAVAILABLE);
-                for(int i=0;i<5;i++)
-                {
-                    Tab tab=new Tab("tab "+(i+1));
-                    HBox hBox=new HBox();
-                    hBox.getChildren().add(new Label("Tab "+(i+1)));
-                    hBox.setAlignment(Pos.TOP_LEFT);
-                    tab.setContent(hBox);
-                    tabPane.getTabs().add(tab);
-                }
-                AnchorPane.setTopAnchor(tabPane,5.0);
-                AnchorPane.setLeftAnchor(tabPane,5.0);
-                AnchorPane.setRightAnchor(tabPane,5.0);
-                AnchorPane.setTopAnchor(backButton,5.0);
-                AnchorPane.setRightAnchor(backButton,5.0);
-                root.getChildren().addAll(tabPane,backButton);
-                primaryStage.setScene(employeesDatabaseScene);
-                sceneStack.push(employeesDatabaseScene);
-            }
-        });
-        root.getChildren().addAll(customerDatabaseButton,booksDatabaseButton,employeesDatabaseButton,titleTextView);
-        primaryStage.setScene(homePage);
-        sceneStack.push(homePage);
-        primaryStage.show();
-    }
-
-    public static void main(String[] args)
-    {
-        launch(args);
+        }
     }
 }
